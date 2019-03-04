c95962e4926b306f7f836c46e18633b4082c54bb
Adams Korir <adekorir@gmail.com>
2019-03-04 12:11:46 +0300

    Removed the testPersonCreation() test.
    The test passed the red-green-refactor steps completely and is no longer needed.
    
    ~ Adams Korir

--- src/test/java/com/adekorir/example/ranking/PersonTest.java
+++ src/test/java/com/adekorir/example/ranking/PersonTest.java
@@ -6,11 +6,8 @@ import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class PersonTest {
 
@@ -35,31 +32,5 @@ public class PersonTest {
 
             tx.commit();
         }
-    }
-
-    @Test
-    public void testPersonCreation() {
-        Person subject = new Person();
-        subject.setName("Adam");
-
-        Person observer = new Person();
-        observer.setName("Eve");
-
-        Skill skill = new Skill();
-        skill.setName("Java");
-
-        Ranking ranking = new Ranking();
-        ranking.setSubject(subject);
-        ranking.setObserver(observer);
-        ranking.setSkill(skill);
-        ranking.setRanking(8);
-
-        String toString = String.format("Subject: %s, Observer: %s, Skill: %s, Ranking: %d",
-                ranking.getSubject().getName(),
-                ranking.getObserver().getName(),
-                ranking.getSkill().getName(),
-                ranking.getRanking());
-
-        assertEquals(toString, ranking.toString());
     }
 }
